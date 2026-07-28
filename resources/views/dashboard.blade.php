b9637a77ea885a3b9e2a39d14d31724ced912f57
Fatwa <fatwazihad222@gmail.com>
2026-07-28 15:24:42 +0700

    Tinggal modul 2 dari awal

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,18 @@
+<!-- memanggil file app.blade.php -->
+@extends('layouts.app')
+
+<!-- mengirinkan nilai ke tittle untuk ditampilkan -->
+@section('title', 'Login')
+
+<!-- batas awal isi konten -->
+@section('content')
+
+
+    <h1>Dashboard</h1>
+    <form action="{{ route('logout') }}" method="POST">
+        @csrf
+        <button type="submit" class="btn btn-danger">Logout</button>
+    </form>
+
+<!-- batas akhir isi konten -->
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
+//route yang bisa diakses ketika user belum login
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'index'])->name('login');
+    Route::post('/auth', [AuthController::class, 'auth'])->name('auth');
 });
-Route::get('/coba', function () {
-    return view('coba');
+
+//route yang bisa diakses ketika user sudah login
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
